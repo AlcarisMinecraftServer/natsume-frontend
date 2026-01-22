66a427128d6d9b6cc0b8b79ee64bbbed224b4d7e
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2026-01-22 20:00:14 +0900

    fix: デザインを統一

--- src/features/common/pages/NotFoundPage.tsx
+++ src/features/common/pages/NotFoundPage.tsx
@@ -1,9 +1,9 @@
 export default function NotFoundPage() {
     return (
-        <div className="flex items-center justify-center h-screen text-white bg-[#1c1e22]">
+        <div className="flex items-center justify-center h-screen bg-[#eef5ff]">
             <div className="text-center space-y-4">
-                <h1 className="text-6xl font-bold">404</h1>
-                <p className="text-xl">ページが見つかりませんでした</p>
+                <h1 className="text-6xl font-bold text-[#080d12]">404</h1>
+                <p className="text-xl text-[#4b5256]">ページが見つかりませんでした</p>
             </div>
         </div>
     );
